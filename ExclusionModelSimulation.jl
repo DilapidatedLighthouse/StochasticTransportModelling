7852eb9c2f2fdaa54dda5b7d8d3f0a5d1c5ec4b0
DilapidatedLighthouse <josiah.s.murray.work@gmail.com>
2023-01-19 23:57:57 +1100

    Update ExclusionModelSimulation.jl

--- ExclusionModelSimulation.jl
+++ ExclusionModelSimulation.jl
@@ -12,11 +12,11 @@ gr() #A graphical thing? I need to look it up
 
 #Declaring variables for simulation
 XLENGTH = 500
-YLENGTH = 50
+YLENGTH = 100
 PROBABILTYOFMOVE = 1.0
 PROBABILTYOFPROLIFERATION = 1.0
 TIMEOFSIMULATION = 0
-NUMBEROFSIMULATIONS = 1
+NUMBEROFSIMULATIONS = 10
 
 #Constants for initial conditions
 H = 50
@@ -32,19 +32,19 @@ xAxisValues = [((-XLENGTH/2):((XLENGTH/2)-1))...]
 #Initialise grid with agents
 simGrid = zeros(XLENGTH,YLENGTH)
 
-simGrid = createBlock(0,2*H, 1000, simGrid,xAxisValues)
+simGrid = createBlockEven(0,2*H, Int(round(YLENGTH/2)), simGrid,xAxisValues)
 center=-150
-simGrid = createBlockEven(center,2*H, Int(round(YLENGTH/2)), simGrid,xAxisValues)
+#simGrid = createBlockEven(center,2*H, YLENGTH, simGrid,xAxisValues)
 #Heat equation. Will have to be manually changed to match the initial conditions and simulation behaviour
 T=TIMEOFSIMULATION
-#C(x)=0.5*(erf((H-x)/sqrt(4*D*T))+erf((H+x)/sqrt(4*D*T)));
+C(x)=0.25*(erf((H-x)/sqrt(4*D*T))+erf((H+x)/sqrt(4*D*T)));
 #C(x)= 0.5*(erf((H-(x-center))/sqrt(4*D*T))+erf((H+(x-center))/sqrt(4*D*T)));
-C(x)=0.5*(erf((H-x)/sqrt(4*D*T))+erf((H+x)/sqrt(4*D*T))) + 0.25*(erf((H-(x-center))/sqrt(4*D*T))+erf((H+(x-center))/sqrt(4*D*T)));
+#C(x)=0.5*(erf((H-x)/sqrt(4*D*T))+erf((H+x)/sqrt(4*D*T))) + 0.25*(erf((H-(x-center))/sqrt(4*D*T))+erf((H+(x-center))/sqrt(4*D*T)));
 
 #Calculate average densities as function of space.
 densities = calculateDensities(StochasticExclusionWalkAverage([XLENGTH, YLENGTH], TIMEOFSIMULATION, simGrid, NUMBEROFSIMULATIONS, PROBABILTYOFMOVE))
 println("Calculations Completed")
 #||||----PLOTS----||||#
 myPlot = scatter(xAxisValues,densities,mc=:black,msc=:match,label="Stochastic")
-myPlot=plot!(C, -XLENGTH/2:XLENGTH/2-1,lw=4,lc=:green,ls=:dash,label="Exact",xlabel="x",ylabel="C(x,500)",xlims=(-XLENGTH/2,XLENGTH/2),ylims=(0,1),framestyle=:box)
+myPlot=plot!(C, -XLENGTH/2:XLENGTH/2-1,lw=4,lc=:green,ls=:dash,label="Exact",xlabel="x",ylabel="C(x, 0)",xlims=(-XLENGTH/2,XLENGTH/2),ylims=(0,1),framestyle=:box)
 display(myPlot)
