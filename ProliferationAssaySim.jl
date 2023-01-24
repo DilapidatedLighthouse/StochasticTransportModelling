21710e769312bd6333b27593e7c24619a9cd6f81
DilapidatedLighthouse <josiah.s.murray.work@gmail.com>
2023-01-24 14:04:41 +1100

    Update ProliferationAssaySim.jl

--- ProliferationAssaySim.jl
+++ ProliferationAssaySim.jl
@@ -1,4 +1,19 @@
+using Plots, SpecialFunctions, Random
+
+#|||---VARIABLES----||||#
+XLENGTH = 500
+YLENGTH = 500
+initialDensity = 0.5
+
+
 #Initialise a grid with randomly placed agents
+
+simGrid = zeros(XLENGTH,YLENGTH)
+for i in simGrid
+    occupiedCheck = rand(1)
+    if
+end#for
+
 
 #Run proliferation simulations. Record averages for a number of timesteps
 
