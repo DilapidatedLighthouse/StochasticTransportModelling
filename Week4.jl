75b338502976303304752e0d0413357182fec287
DilapidatedLighthouse <josiah.s.murray.work@gmail.com>
2023-02-03 14:28:16 +1100

    Create Week4.jl

---
+++ Week4.jl
@@ -0,0 +1,25 @@
+#Grid parameters
+XLENGTH = 500
+YLENGTH = 50
+
+NUMBEROFSIMULATIONS = 10
+STEPSIZE = 1 
+
+
+
+#Population 1 parameters
+
+#Population 2 parameters
+
+
+#Set up initial conditions
+
+
+#Run simulation for two populations
+function simulationForTwoPopulations()
+    return "simgGrid"
+end#function
+
+#Numerical solution for two populations
+
+#Plot the two together
