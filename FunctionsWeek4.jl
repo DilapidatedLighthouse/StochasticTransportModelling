888f3c7aef4b347d056603ebf54623e7d0c5d8f2
DilapidatedLighthouse <josiah.s.murray.work@gmail.com>
2023-02-11 16:50:11 +1100

    Julia Is Stupid
    I am also stupid. Was using my own check function wrong. But Julia was changing a variable for apparently no reason. I still don't understand.... Changed the variable name and now it's working. It'll have to do.

---
+++ FunctionsWeek4.jl
@@ -0,0 +1,126 @@
+function CheckCellEmpty(coordinates, simGridsTemp3)
+    for gridIndex in eachindex(simGridsTemp3)
+        if(Int(simGridsTemp3[gridIndex][coordinates...]) == 1)
+            
+            return gridIndex
+        end#if
+    end#for
+    return 0
+end#Function
+
+function moveAgentUpdate(simGridTemp5, intCoordinates, moveCoord)
+    println(simGridTemp5[(moveCoord)...])
+    simGridTemp5[(moveCoord)...]=1.0
+    # println("Before: ", sum(simGridTemp5[:,2]), "Move: ", moveCoord)
+    simGridTemp5[intCoordinates...] = 0.0
+    # println("After: ", sum(simGridTemp5[:,2]),"Int: ", intCoordinates)
+    return simGridTemp5
+end#function
+
+
+function attemptActionWithDirectionMultiPopulation(moveDirection, simGridsTemp2, actingIndex, coordinates,action)
+    moveVector = zeros(length(coordinates)) #Used to move in the right direction
+
+    #set move vector based on input direction
+    moveVector = [Int(round(cos(pi/2*(2-moveDirection)))),Int(round(sin(pi/2*(2-moveDirection))))]
+
+
+
+    #Point to move to. Converts each element into an integer so it doesn't break when accessing the index.
+    moveCoord = [Int((coordinates+moveVector)[i]) for i in eachindex(coordinates+moveVector)]
+    #Point to move to if movement reflected. Used for boundary conditions. Converts each element into an integer so it doesn't break when accessing the index.
+    oppositeMoveCoord = [Int((coordinates-moveVector)[i]) for i in eachindex(coordinates-moveVector)]
+    #converts coordinates to integers
+    intCoordinates = [Int(coordinates[i]) for i in eachindex(coordinates)]
+
+    #Check to see if move is within bounds
+    if(checkInBoundaries(simGridsTemp2[actingIndex],moveCoord))
+
+        #Check not moving to an occupied space
+        if(CheckCellEmpty(moveCoord, simGridsTemp2)==0)
+            
+            simGridsTemp2[actingIndex] =action(copy(simGridsTemp2[actingIndex]),intCoordinates,moveCoord)
+            
+        end#if
+    
+    #Otherwise bounce off the boundary (naive. just a move in the other direction)
+    elseif(checkInBoundaries(simGridsTemp2[actingIndex],oppositeMoveCoord))
+        #Check not moving to an occupied space
+        if(CheckCellEmpty(oppositeMoveCoord, simGridsTemp2)==0)
+            #=
+            simGrid[(oppositeMoveCoord)...]=1.0
+            simGrid[intCoordinates...] = 0.0
+            =#
+            simGridsTemp2[actingIndex] = action(copy(simGridsTemp2[actingIndex]),intCoordinates,oppositeMoveCoord)
+        end#if
+    end#if elseif
+    return simGridsTemp2
+end#Function
+
+
+
+#Run simulation for two populations. Here we run each population on its own grid. The function just makes sense on all simgGrids. 
+function StochasticExclusionWalkAverageMultTimesMultiplePopulations(lengths, times, simGridsTemp, numSimultaions, probMovements, biases)
+    totalAgents = sum(sum(simGridsTemp)) #We sum over each simGrid and then over all simgrids
+    sumGrids = map(x -> fill(zeros(lengths...),length(times)), simGridsTemp)#The result of each simulation will be added to this variable so it can be averaged later
+    maxTime = maximum(times)
+    
+    for sim in 1:numSimultaions
+        println("Simulation: ",sim)#Print the current number of simulations
+
+        tempGrids = copy(simGridsTemp)
+
+        for t in 0:maxTime
+            
+            count=0
+            
+            #Move the agents
+            while count < totalAgents #choose random cell to try to find an agent to move
+               randCoordinates = []
+               for i in eachindex(lengths)
+                    append!(randCoordinates,rand(1:lengths[i]))
+               end#for
+
+               #Check if the cell contains an agentc
+               occupiedIndex = CheckCellEmpty(randCoordinates, simGridsTemp)
+               if(occupiedIndex != 0)
+                
+                    count+=1
+                    #Does it move?
+                    moveQuery = rand(1)[1]
+                    if(moveQuery <= probMovements[occupiedIndex])
+                        moveDirection = chooseDirectionWithXBias(biases)
+                        
+                        #Move the agent
+                        
+                        tempGrids = attemptActionWithDirectionMultiPopulation(moveDirection,tempGrids, occupiedIndex, randCoordinates, moveAgentUpdate)
+                        
+                    end#if
+                end#if
+            end#while
+            
+            if(t in times)
+                
+                for i in eachindex(probMovements) #dumb way of getting number of populations
+                    # println("sumGrids, size = ", size(sumGrids),": ", typeof(sumGrids))
+                    # println("tempGrids, size = ", size(tempGrids),": ", typeof(tempGrids))
+
+                    sumGrids[i][findlast(time -> time==t, times)]+=tempGrids[i]
+                end
+            end#if
+
+        end#for
+        
+        # #Sum the results of all simulations
+        
+        # sumGrid+=tempGrid
+        
+    end#for
+    
+    #Average placement of agents across the grid throughout all simulations
+    averageGrid = sumGrids/numSimultaions
+    
+
+    return averageGrid
+
+end#function
--- Week4.jl
+++ Week4.jl
@@ -1,148 +1,70 @@
 using Plots, SpecialFunctions, Random, DifferentialEquations
 include("functions.jl")
-
+include("functionsWeek4.jl")
 
 
 
 
 #Grid parameters
-XLENGTH = 500
-YLENGTH = 50
+XLENGTH = 10
+YLENGTH = 10
 lengths = [XLENGTH, YLENGTH]
 xAxisValues = [((-XLENGTH/2):((XLENGTH/2)-1))...]
-NUMBEROFSIMULATIONS = 10
+NUMBEROFSIMULATIONS = 1
 STEPSIZE = 1 
 
-times = [0,2]
+times = [0]
 NUMBEROFSIMULATIONS = 1
 biases = 0 #not properly implemented
 
 probMovements = [1,1]
+#if one of the grids contain a cell at the specified coordinates, the index of that grid will be returned. Else the function will return 0.
 
 #Set up initial conditions
+function createBlock(center, width, height, simGrid, xAxisValues)
 
+    #ensure the height of the block is at most the height of the grid
+    if(height > size(simGrid,2))
+        height = size(simGrid,2)
+    end#if
+    
+    #ensure the width of the block is at most the width of the grid. (That'd be a borring simulation, but you do you.)
+    if(width > size(simGrid,1))
+        width = size(simGrid,1)
+    end#if
+
+    #place the block
+    for i in axes(simGrid,1)
+        if(abs(xAxisValues[i]-center) <= width/2)
+            for j in 1:height
+                simGrid[i,j]=1.0
+            end#for
+        end#if
+    end#for
+    
+    return simGrid
+end#function
 #population 1
-H = 25
+H = 2
 simGrid1 = zeros(XLENGTH,YLENGTH)
 
-simGrid1 = createBlockEven(-100,2*H, 100000, simGrid1,xAxisValues)
+simGrid1 = createBlock(0,2*H, 100000, simGrid1,xAxisValues)
 #population 2
 H = 25
 simGrid2 = zeros(XLENGTH,YLENGTH)
 
-simGrid2 = createBlockEven(100,2*H, YLENGTH, simGrid2,xAxisValues)
+simGrid2 = createBlock(100,2*H, YLENGTH, simGrid2,xAxisValues)
 
-simGrids = [simGrid1, simGrid2]
-
-#Run simulation for two populations. Here we run each population on its own grid. The function just makes sense on all simgGrids. 
-function StochasticExclusionWalkAverageMultTimesMultiplePopulations(lengths, times, simGrids, numSimultaions, probMovements, biases)
-    totalAgents = sum(sum(simGrids)) #We sum over each simGrid and then over all simgrids
-    sumGrids = map(x -> fill(zeros(lengths...),length(times)), simGrids)#The result of each simulation will be added to this variable so it can be averaged later
-    maxTime = maximum(times)
-
-    for sim in 1:numSimultaions
-        println("Simulation: ",sim)#Print the current number of simulations
-
-        tempGrids = copy(simGrids)
-
-        for t in 0:maxTime
-            
-            count=0
-            
-            #Move the agents
-            while count < totalAgents #choose random cell to try to find an agent to move
-               randCoordinates = []
-               for i in eachindex(lengths)
-                    append!(randCoordinates,rand(1:lengths[i]))
-               end#for
-
-               #Check if the cell contains an agentc
-               occupiedIndex = CheckCellEmpty(randCoordinates, simGrids)
-               if(occupiedIndex != 0)
-                    count+=1
-                    #Does it move?
-                    moveQuery = rand(1)[1]
-                    if(moveQuery <= probMovements[occupiedIndex])
-                        moveDirection = chooseDirectionWithXBias(biases)
-
-                        #Move the agent
-                        tempGrids = attemptActionWithDirectionMultiPopulation(moveDirection,tempGrids, occupiedIndex, randCoordinates, moveAgent)
-                    end#if
-                end#if
-            end#while
-            if(t in times)
-                for i in eachindex(probMovements) #dumb way of getting number of populations
-                    # println("sumGrids, size = ", size(sumGrids),": ", typeof(sumGrids))
-                    # println("tempGrids, size = ", size(tempGrids),": ", typeof(tempGrids))
-
-                    sumGrids[i][findlast(time -> time==t, times)]+=tempGrids[i]
-                end
-            end#if
-
-        end#for
-        
-        # #Sum the results of all simulations
-        
-        # sumGrid+=tempGrid
-        
-    end#for
-    
-    #Average placement of agents across the grid throughout all simulations
-    averageGrid = sumGrids/numSimultaions
-    
-
-    return averageGrid
-
-end#function
-
-function attemptActionWithDirectionMultiPopulation(moveDirection, simGrids, actingIndex, coordinates,action)
-    moveVector = zeros(length(coordinates)) #Used to move in the right direction
-
-    #set move vector based on input direction
-    moveVector = [Int(round(cos(pi/2*(2-moveDirection)))),Int(round(sin(pi/2*(2-moveDirection))))]
+simGridsMaster = [copy(simGrid1), copy(simGrid2)]
 
 
 
-    #Point to move to. Converts each element into an integer so it doesn't break when accessing the index.
-    moveCoord = [Int((coordinates+moveVector)[i]) for i in eachindex(coordinates+moveVector)]
-    #Point to move to if movement reflected. Used for boundary conditions. Converts each element into an integer so it doesn't break when accessing the index.
-    oppositeMoveCoord = [Int((coordinates-moveVector)[i]) for i in eachindex(coordinates-moveVector)]
-    #converts coordinates to integers
-    intCoordinates = [Int(coordinates[i]) for i in eachindex(coordinates)]
 
-    #Check to see if move is within bounds
-    if(checkInBoundaries(simGrids[actingIndex],moveCoord))
 
-        #Check not moving to an occupied space
-        if(CheckCellEmpty(moveCoord, simGrids)!=0)
-            simGrids[actingIndex] =action(simGrids[actingIndex],intCoordinates,moveCoord)
-        end#if
-    
-    #Otherwise bounce off the boundary (naive. just a move in the other direction)
-    elseif(checkInBoundaries(simGrids[actingIndex],oppositeMoveCoord))
-        #Check not moving to an occupied space
-        if(CheckCellEmpty(oppositeMoveCoord, simGrids)!=0)
-            #=
-            simGrid[(oppositeMoveCoord)...]=1.0
-            simGrid[intCoordinates...] = 0.0
-            =#
-            simGrids[actingIndex] = action(simGrids[actingIndex],intCoordinates,oppositeMoveCoord)
-        end#if
-    end#if elseif
-    return simGrids
-end#Function
 
-#if one of the grids contain a cell at the specified coordinates, the index of that grid will be returned. Else the function will return 0.
-function CheckCellEmpty(coordinates, simGrids)
-    for gridIndex in eachindex(simGrids)
-        if(simGrids[gridIndex][coordinates...] == 1)
-            return gridIndex
-        end#if
-    end#for
-    return 0
-end#Function
 
-simulations = StochasticExclusionWalkAverageMultTimesMultiplePopulations(lengths, times, simGrids, NUMBEROFSIMULATIONS, probMovements, biases)
+simulations = StochasticExclusionWalkAverageMultTimesMultiplePopulations(lengths, times, copy(simGridsMaster), NUMBEROFSIMULATIONS, probMovements, biases)
+
 densities = map(sim -> map(pop -> calculateDensities(pop), sim), simulations)
 
 scatter(xAxisValues, densities[1][1])
